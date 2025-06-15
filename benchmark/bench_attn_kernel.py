8b4819783c32b67627a3a2e7ed613c3ac6fc008f
haochengxi <328659100@qq.com>
2025-06-15 09:19:14 +0800

    add flashinfer attn benchmark

--- benchmark/bench_attn_kernel.py
+++ benchmark/bench_attn_kernel.py
@@ -1,31 +1,41 @@
 import argparse
 import math
+import warnings
+    
 import torch
 import triton
 import triton.language as tl
 from collections import defaultdict
 
-from flash_attn.flash_attn_interface import flash_attn_func as flash2
-from flash_attn_interface import flash_attn_func as flash3
+try:
+    from flash_attn.flash_attn_interface import flash_attn_func as flash2
+    from flash_attn_interface import flash_attn_func as flash3
+except:
+    warnings.warn("flash_attn is not installed")
+
+try:
+    import flashinfer
+except:
+    warnings.warn("flashinfer is not installed")
 
 from coat.attention.triton_attn import attention_forward
 from coat.attention.triton_attn_tma import tma_attention_forward
 
 # Supported methods
 BENCH_CONFIGS = {
-    "flash2-bf16": {
-        "line_vals": "flash2-bf16",
-        "line_names": "Flash2-BF16",
+    "flashattn2-bf16": {
+        "line_vals": "flashattn2-bf16",
+        "line_names": "FlashAttn2-BF16",
         "color": ("red", "-"),
     },
-    "flash3-bf16": {
-        "line_vals": "flash3-bf16",
-        "line_names": "Flash3-BF16",
+    "flashattn3-bf16": {
+        "line_vals": "flashattn3-bf16",
+        "line_names": "FlashAttn3-BF16",
         "color": ("blue", "-"),
     },
-    "flash3-fp8": {
-        "line_vals": "flash3-fp8",
-        "line_names": "Flash3-FP8",
+    "flashattn3-fp8": {
+        "line_vals": "flashattn3-fp8",
+        "line_names": "FlashAttn3-FP8",
         "color": ("purple", "-"),
     },
     "triton-bf16": {
@@ -47,6 +57,26 @@ BENCH_CONFIGS = {
         "line_vals": "triton-fp8-tma",
         "line_names": "Triton-FP8-TMA",
         "color": ("pink", "-"),
+    },
+    "flashinfer2-bf16": {
+        "line_vals": "flashinfer2-bf16",
+        "line_names": "FlashInfer2-BF16",
+        "color": ("brown", "-"),
+    },
+    "flashinfer3-bf16": {
+        "line_vals": "flashinfer3-bf16",
+        "line_names": "FlashInfer3-BF16",
+        "color": ("brown", "-"),
+    },
+    "flashinfer2-fp8": {
+        "line_vals": "flashinfer2-fp8",
+        "line_names": "FlashInfer2-FP8",
+        "color": ("brown", "-"),
+    },
+    "flashinfer3-fp8": {
+        "line_vals": "flashinfer3-fp8",
+        "line_names": "FlashInfer3-FP8",
+        "color": ("brown", "-"),
     },
 }
 
@@ -70,15 +100,17 @@ def benchmarker(seqlen, batch_size, model_dim, dropout_p, headdim, provider, gen
     v_descale = torch.randn((batch_size, nheads), device="cuda", dtype=torch.float32)
 
     quantiles = [0.5, 0.2, 0.8]
-    if "flash" in provider:
+    if "flashattn" in provider:
         # Input is (B, N, H, D)
-        if provider == "flash2-bf16":
+        if provider == "flashattn2-bf16":
             ms, min_ms, max_ms = triton.testing.do_bench(lambda: flash2(q, k, v, dropout_p, causal=False), quantiles=quantiles, rep=500)
-        elif provider == "flash3-bf16":
+        elif provider == "flashattn3-bf16":
             ms, min_ms, max_ms = triton.testing.do_bench(lambda: flash3(q, k, v, causal=False), quantiles=quantiles, rep=500)
-        elif provider == "flash3-fp8":
+        elif provider == "flashattn3-fp8":
             assert q.dtype in [torch.float8_e5m2, torch.float8_e4m3fn]
             ms, min_ms, max_ms = triton.testing.do_bench(lambda: flash3(q, k, v, causal=False, q_descale=q_descale, k_descale=k_descale, v_descale=v_descale), quantiles=quantiles, rep=500)
+        else:
+            raise ValueError(f"FlashAttn Invalid provider: {provider}")
     elif "triton" in provider:
         # Input is (B, H, N, D)
         q, k, v = q.transpose(1, 2).contiguous(), k.transpose(1, 2).contiguous(), v.transpose(1, 2).contiguous()
@@ -93,8 +125,30 @@ def benchmarker(seqlen, batch_size, model_dim, dropout_p, headdim, provider, gen
             ms, min_ms, max_ms = triton.testing.do_bench(lambda: attention_forward(q, k, v, causal=False, sm_scale=1.0 / math.sqrt(headdim)), quantiles=quantiles, rep=500)
         elif provider == "triton-fp8-tma":
             ms, min_ms, max_ms = triton.testing.do_bench(lambda: tma_attention_forward(q, k, v, causal=False, sm_scale=1.0 / math.sqrt(headdim)), quantiles=quantiles, rep=500)
+        else:
+            raise ValueError(f"Triton Invalid provider: {provider}")
+    elif "flashinfer" in provider:
+        # Input is (N, H, D)
+        def reshape_func(tensor):
+            return tensor.permute(1, 0, 2, 3).reshape(seqlen, batch_size * nheads, headdim).contiguous()
+        q, k, v = reshape_func(q), reshape_func(k), reshape_func(v)
+        
+        def reshape_scale_func(tensor):
+            return tensor.flatten().contiguous()
+        q_descale, k_descale, v_descale = reshape_scale_func(q_descale), reshape_scale_func(k_descale), reshape_scale_func(v_descale)
+        
+        if provider == "flashinfer2-bf16":
+            ms, min_ms, max_ms = triton.testing.do_bench(lambda: flashinfer.single_prefill_with_kv_cache(q, k, v, causal=False, return_lse=False, backend="fa2"), quantiles=quantiles, rep=500)
+        elif provider == "flashinfer3-bf16":
+            ms, min_ms, max_ms = triton.testing.do_bench(lambda: flashinfer.single_prefill_with_kv_cache(q, k, v, causal=False, return_lse=False, backend="fa3"), quantiles=quantiles, rep=500)
+        elif provider == "flashinfer2-fp8":
+            raise NotImplementedError("FlashInfer2-FP8 is not supported in FlashInfer")
+        elif provider == "flashinfer3-fp8":
+            ms, min_ms, max_ms = triton.testing.do_bench(lambda: flashinfer.single_prefill_with_kv_cache(q, k, v, causal=False, return_lse=False, backend="fa3", scale_q=q_descale, scale_k=k_descale, scale_v=v_descale), quantiles=quantiles, rep=500)
+        else:
+            raise ValueError(f"FlashInfer Invalid provider: {provider}")
     else:
-        raise ValueError(f"Invalid provider: {provider}")
+        raise ValueError(f"General Invalid provider: {provider}")
 
     flops = compute_flops(batch_size, nheads, seqlen, seqlen, headdim, headdim, causal=False)
     tflops = flops / (ms * 1e9)
@@ -107,7 +161,7 @@ if __name__ == "__main__":
     parser.add_argument(
         '--methods', 
         nargs='+', 
-        default=['flash2-bf16', 'flash3-bf16'],
+        default=['flashattn2-bf16', 'flashattn3-bf16'],
         help='Methods to benchmark.'
     )
     args = parser.parse_args()
